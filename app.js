bba3cc81e9f574aaa1a308522d8d3d812eb8f812
Billy Lynch <wlynch92@gmail.com>
2013-11-19 18:26:52 -0500

    Added stops query

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var nextbus = require('nextbusjs').client();
 var app = express();
-app.use(express.static('public'));
 
+app.use(express.static('public'));
 function getStops(lat, lon, callback) {
+	//TODO: Cache the agency so that we don't have to keep getting this MASSIVE file
 	nextbus.cacheAgency('rutgers', function (err) {
 		if (err) {
 			throw err;
@@ -17,9 +18,28 @@ app.get('/hello.txt', function(req, res){
 	res.send('Hello world!' + req.query.x);
 });
 
-app.get('/nextbus', function(req, res){
+app.get('/api/nextbus/stops', function(req, res){
 	getStops(req.query.lat, req.query.lon, function(result){ 
 		res.send(result) 
+	});
+});
+
+app.get('test', function(req, res){
+	res.send(req.query.test);
+});
+
+app.get('/api/nextbus/times', function(req, res){
+	console.log(req.query.stop);
+	nextbus.getAgencyCache('rutgers', function(err) {
+		nextbus.stopPredict(
+			req.query.stop, 
+			null, 
+			function(err, data) { 
+				console.log(data);
+				res.send(data);
+			}, 
+			'minutes'
+		);
 	});
 });
 

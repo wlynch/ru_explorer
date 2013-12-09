972513268a301877b4b5ebc0fa7cc0ca30f04923
Billy Lynch <wlynch92@gmail.com>
2013-12-08 20:50:33 -0500

    Removed empty routes

--- bus.py
+++ bus.py
@@ -2,10 +2,26 @@ import requests
 
 nextbusURL="http://runextbus.herokuapp.com"
 
+# Removes routes that are currently not in service 
+def prune_routes(routes):
+	for route in routes:
+		empty = True
+		remove = []
+		for stop in routes[route]:
+			if stop["predictions"] is not None:
+				empty = False
+			else:
+				remove.append(stop)
+		for stop in remove:
+			routes[route].remove(stop)
+		if empty is True:
+			routes.pop(route)
+	return routes
+
 def get_nearby(lat, lon):
 	stops = requests.get(nextbusURL + "/nearby/" + str(lat) + "/" + str(lon)).json()
 	retval = {}
 	for stop in stops:
 		tmp = requests.get(nextbusURL + "/stop/" + stop).json()
 		retval[stop] = tmp
-	return retval
+	return prune_routes(retval)
